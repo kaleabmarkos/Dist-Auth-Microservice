d873322e5637dc2818b1694bdbd16d257cbd8eeb
kaleabmarkos <kaleabgessese@gmail.com>
2025-04-05 22:02:22 -0500

    password hash

--- auth-service/utils/hash.go
+++ auth-service/utils/hash.go
@@ -1,2 +1,13 @@
 package utils
 
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hashed_bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed_bytes), err
+}
+
+func CheckPassword(password, hash string) bool{
+	err:=bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
