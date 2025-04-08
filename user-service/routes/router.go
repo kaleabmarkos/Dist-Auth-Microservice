b645d97ee1a43d16b78028193d3f6a2ba8d492f2
kaleabmarkos <kaleabgessese@gmail.com>
2025-04-08 16:29:24 -0500

    router

--- user-service/routes/router.go
+++ user-service/routes/router.go
@@ -1,10 +1,22 @@
 package routes
 
 import (
+	"net/http"
+	"Dist-Auth-MicroService/user-service/handlers"
+	"Dist-Auth-MicroService/user-service/middleware"
+	
+
 	"github.com/go-chi/chi/v5"
 )
 
-func NewRouter(){
+func NewRouter() http.Handler{
 	r:=chi.NewRouter()
+	r.Use(middleware.AuthMiddleware)
+
+	r.Post("/", handlers.CreateUser)
+	r.Get("/{id}", handlers.GetUserById)
+	r.Put("/{id}/role", handlers.UpdateUserRole)
+
+	return r
 
 }
