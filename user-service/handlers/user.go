40d11f194f283c70beba4776af0b85c1218d65ab
kaleabmarkos <kaleabgessese@gmail.com>
2025-04-08 13:43:33 -0500

    create user req

--- user-service/handlers/user.go
+++ user-service/handlers/user.go
@@ -1,12 +1,32 @@
 package handlers
 
-import(
+import (
+	"Dist-Auth-MicroService/shared/db"
+	"Dist-Auth-MicroService/user-service/models"
+	"context"
+	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request){
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	user.CreateAt = time.Now()
 
+	collection := db.GetCollection("users")
 
+	res, err := collection.InsertOne(context.TODO(), user)
+	if err!=nil{
+		http.Error(w, "Failed to create the user", http.StatusInternalServerError)
+	}
+
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	json.NewEncoder(w).Encode(user)
 }
 
 func GetUserById(w http.ResponseWriter, r *http.Request){
