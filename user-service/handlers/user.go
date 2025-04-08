9c571e5f59b03a801ca68bef6c1948097bf27198
kaleabmarkos <kaleabgessese@gmail.com>
2025-04-07 20:49:38 -0500

    user handlers

---
+++ user-service/handlers/user.go
@@ -0,0 +1,20 @@
+package handlers
+
+import(
+	"net/http"
+)
+
+func CreateUser(w http.ResponseWriter, r *http.Request){
+
+
+}
+
+func GetUserById(w http.ResponseWriter, r *http.Request){
+
+
+}
+
+func UpdateUserRole(w http.ResponseWriter, r *http.Request){
+
+
+}
